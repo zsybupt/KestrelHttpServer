bd50768e4f02fe9b356a5bcc7feda656e12fbe6e
Stephen Halter <halter73@gmail.com>
2018-04-05 02:02:06 -0700

    baz

---
+++ src/Kestrel.Transport.Sockets/Internal/PipeScheduelrAwaitable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Internal
+{
+    public class PipeSchedulerAwiatable : ICriticalNotifyCompletion
+    {
+        private readonly PipeScheduler _ioScheduler;
+
+        public PipeSchedulerAwiatable(PipeScheduler ioScheduler)
+        {
+            _ioScheduler = ioScheduler;
+        }
+
+        public PipeSchedulerAwiatable GetAwaiter() => this;
+        public bool IsCompleted => false;
+
+        public void GetResult()
+        {
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            UnsafeOnCompleted(continuation);
+        }
+
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            _ioScheduler.Schedule(state => ((Action)state)(), continuation);
+        }
+    }
+}
